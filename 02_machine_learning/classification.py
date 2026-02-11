d2b021e33eeb12caa45c75d6c2a56c262e0127be
Soumya Shaha <shahasoumya4@gmail.com>
2026-02-11 16:30:27 +0530

    Updated README for Day 3

---
+++ 02_machine_learning/classification.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Step 1: Create dataset
+data = {
+   "hours": [1,2,3,4,5,6,7,8,9,10],
+   "pass":  [0,0,0,1,1,1,1,1,1,1]
+
+}
+
+df = pd.DataFrame(data)
+
+# Step 2: Features and labels
+X = df[["hours"]]
+y = df["pass"]
+
+# Step 3: Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42
+)
+
+# Step 4: Train model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Step 5: Predict
+predictions = model.predict(X_test)
+
+# Step 6: Accuracy
+accuracy = accuracy_score(y_test, predictions)
+print("Model accuracy:", accuracy)
+
+# Step 7: Test with new data
+new_hours = [[2]]
+new_hours = [[7]]
+
+result = model.predict(new_hours)
+
+if result[0] == 1:
+    print("Prediction: Pass")
+else:
+    print("Prediction: Fail")
